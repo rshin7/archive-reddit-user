b69cafb56a1cb5471a5cf2197e369e06ede82b36
Richard Shin <rs@moogra.com>
2023-08-11 22:59:16 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="archive_reddit_user",
-    version="0.11",
+    version="0.12",
     packages=find_packages(),
     install_requires=[
         "praw",
@@ -11,9 +15,13 @@ setup(
     author="Richard Shin",
     author_email="webmaster@moogra.com",
     description="A tool to archive Reddit user comments",
-    keywords="reddit archiver comments backup",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="reddit, archiver, comments, backup",
     url="https://github.com/rshin7/archive-reddit-user",
     classifiers=[
+        "Development Status :: 4 - Beta"
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
@@ -22,5 +30,9 @@ setup(
         'console_scripts': [
             'archive-reddit-user=archive_reddit_user.main:main',
         ],
+    },
+    project_urls={  
+        "Bug Reports": "https://github.com/rshin7/archive-reddit-user/issues",
+        "Source": "https://github.com/rshin7/archive-reddit-user/",
     },
 )
