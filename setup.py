0bf9391f0dee6f62bf047fd10460f5f59bee7670
Richard Shin <rs@moogra.com>
2023-08-12 00:02:16 -0400

    test integration with github actions

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="archive_reddit_user",
-    version="0.12",
+    version="0.13",
     packages=find_packages(),
     install_requires=[
         "praw",
